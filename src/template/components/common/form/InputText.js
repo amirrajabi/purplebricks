fa84a5b3bea4b5d2a2f0c0943f1295181f4961d5
Amir Rajabi <amir@amirrajabi.com>
2017-05-10 23:48:40 +1000

    Add basic style ReOpenNegotiations component

--- src/template/components/common/form/InputText.js
+++ src/template/components/common/form/InputText.js
@@ -1,3 +1,17 @@
 /**
  * Created by Amir on 9/5/17.
  */
+
+import React, {PropTypes} from 'react';
+
+const InputText = ({className}) => {
+    return (
+        <input className={className}/>
+    );
+};
+
+InputText.propTypes = {
+    className: PropTypes.string
+};
+
+export default InputText;
--- src/template/layouts/secure-negotiation-centre-page/components/ReOpenNegotiations.js
+++ src/template/layouts/secure-negotiation-centre-page/components/ReOpenNegotiations.js
@@ -2,13 +2,49 @@
  * Created by Amir on 9/5/17.
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Body from '../../../components/common/typography/Body';
+import Button from '../../../components/common/button/TextButton';
+import InputText from '../../../components/common/form/InputText';
 
 class ReOpenNegotiations extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            negotiateClassName: 'typography--purple',
+            negotiateText: 'Would you like your Expert, Robert White, to negotiate on your behalf? It’s completely free!',
+        };
+    }
+
     render() {
         return (
-            <section>
-                Reopen Negotiations
+            <section className="reopen-negotiations box--large box--purple">
+
+                <div className="box--small box--gray">
+                    <div className="row">
+                        <div className="col-sm-8">
+                            <Body className={this.state.negotiateClassName}
+                                  text={this.state.negotiateText}/>
+                        </div>
+                        <div className="col-sm-4 reopen-negotiations__negotiate">
+                            <Button className="btn btn--purple"
+                                    text="negotiate for me"/>
+                        </div>
+                    </div>
+                </div>
+
+                <Body className="typography--purple typography--bold layout--l-s-g"
+                      text="Enter an offer you would be willing to accept:"/>
+
+                <Body className="typography--gray-dark typography--regular typography--copy-lg layout--inline layout--auto-height"
+                      text="£"/>
+
+                <InputText className="reopen-negotiations__input-offer layout--inline"/>
+
+                <Button className="reopen-negotiations__submit-btn btn btn--blue"
+                        text="submit offer"/>
+
             </section>
         );
     }
