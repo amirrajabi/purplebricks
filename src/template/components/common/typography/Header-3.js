b0ecdfdfc9de428b006b960330299e50c6a35e6c
Amir Rajabi <amir@amirrajabi.com>
2017-05-11 23:46:00 +1000

    Update Style for Re-open negotiation component

--- src/template/components/common/typography/Header-3.js
+++ src/template/components/common/typography/Header-3.js
@@ -1,3 +1,20 @@
 /**
  * Created by Amir on 9/5/17.
  */
+
+import React, {PropTypes} from 'react';
+
+const Header3 = ({className, text}) => {
+    return (
+        <h3 className={className}>
+            {text}
+        </h3>
+    );
+};
+
+Header3.propTypes = {
+    className: PropTypes.string,
+    text: PropTypes.string.isRequired
+};
+
+export default Header3;
--- src/template/layouts/secure-negotiation-centre-page/components/ReOpenNegotiations.js
+++ src/template/layouts/secure-negotiation-centre-page/components/ReOpenNegotiations.js
@@ -3,7 +3,9 @@
  */
 
 import React, {Component} from 'react';
+import Header from '../../../components/global/BoxHeader';
 import Body from '../../../components/common/typography/Body';
+import Title from '../../../components/common/typography/Header-3';
 import Button from '../../../components/common/button/TextButton';
 import InputText from '../../../components/common/form/InputText';
 
@@ -12,7 +14,6 @@ class ReOpenNegotiations extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            negotiateClassName: 'typography--purple',
             negotiateText: 'Would you like your Expert, Robert White, to negotiate on your behalf? It’s completely free!',
         };
     }
@@ -21,10 +22,13 @@ class ReOpenNegotiations extends Component {
         return (
             <section className="reopen-negotiations box--large box--purple">
 
+                <Header className="typography--purple"
+                        text="Re-open negotiations"/>
+
                 <div className="box--small box--gray">
                     <div className="row">
                         <div className="col-sm-8">
-                            <Body className={this.state.negotiateClassName}
+                            <Body className="reopen-negotiations__negotiate-text typography--purple"
                                   text={this.state.negotiateText}/>
                         </div>
                         <div className="col-sm-4 reopen-negotiations__negotiate">
@@ -34,16 +38,23 @@ class ReOpenNegotiations extends Component {
                     </div>
                 </div>
 
-                <Body className="typography--purple typography--bold layout--l-s-g"
-                      text="Enter an offer you would be willing to accept:"/>
+                <div className="clearfix">
+                    <Title className="typography--purple typography--bold layout--l-s-g"
+                           text="Enter an offer you would be willing to accept:"/>
 
-                <Body className="typography--gray-dark typography--regular typography--copy-lg layout--inline layout--auto-height"
-                      text="£"/>
+                    <Body className="reopen-negotiations__currency typography--gray-dark layout--inline"
+                          text="£"/>
 
-                <InputText className="reopen-negotiations__input-offer layout--inline"/>
+                    <InputText className="reopen-negotiations__input-offer layout--inline"/>
 
-                <Button className="reopen-negotiations__submit-btn btn btn--blue"
-                        text="submit offer"/>
+                    <Button className="reopen-negotiations__submit-btn btn btn--blue"
+                            text="submit offer"/>
+                </div>
+
+                <div className="layout--l-s-g">
+                    <Button className="reopen-negotiations__add-comments btn btn--purple"
+                            text="add comments"/>
+                </div>
 
             </section>
         );
